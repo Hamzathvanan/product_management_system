0b5e36237eb61b75d82c3e5a1764b17af989cce2
Hamzathvanan <hamzathvanan@gmail.com>
2023-06-19 14:44:53 +0530

    Update 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import domain.Product;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello!! world!");
+
+        Product p = new Product();
+        System.out.println(p);
 
     }
 }
